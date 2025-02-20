e4b5d2ceb6db61891566b4456973cdd522bd0533
Samarth M R <samarth.m.r@maersk.com>
2025-02-20 15:53:02 +0530

    routing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import CxAgentMva from "@/components/virtual-assistants/cx-agent-mva/CxAgentMva.vue";
 import Header from "@/components/header/Header.vue";
 import Sidebar from "@/components/sidebar/Sidebar.vue";
 import { ref } from 'vue';
@@ -13,14 +12,11 @@ function toggleSidebar() {
 
 <template>
   <div class="h-screen w-full flex flex-col">
-    <!-- Header -->
     <Header @toggle-sidebar="toggleSidebar" />
     <div class="flex flex-1">
-      <!-- Sidebar -->
       <Sidebar :is-expanded="isSidebarExpanded" />
-      <!-- Main Content -->
       <div class="flex-1 flex flex-col bg-gray-50 transition-all duration-300">
-        <CxAgentMva />
+        <router-view></router-view>
       </div>
     </div>
   </div>
